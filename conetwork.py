0d5a54d529b16b488497af46c9ff98d179b3a526
Shellet <emma.prudent@laposte.net>
2014-12-10 17:43:58 +0100

    Graphe conetwork

---
+++ conetwork.py
@@ -0,0 +1,21 @@
+import networkx as nx
+import random
+import matplotlib.pyplot as plt
+
+class CoNet() :
+
+	def __init__(self,M) :
+		""" Init co occurence graph """
+		self.M = M
+		self.G = self.construct_graph()
+	
+	def construct_graph(self) : 
+		""" Construct graph from matrix """
+		return nx.from_numpy_matrix(self.M)
+
+	def show(self):
+		""" Plot the graph"""
+		nx.draw(self.G)  
+		plt.draw()
+		plt.show()
+		
